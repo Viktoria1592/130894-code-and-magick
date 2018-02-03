ffb2fab915e6e311fcdaf954e8fe90058e90a3f2
Keks <keks@htmlacademy.ru>
2018-02-03 20:51:10 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/setup.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+var WIZARD_QUANTITY = 4;
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizards = [];
+
+var newWizard;
+
+var generateRandomParameter = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var generateRandomAppearance = function (arr) {
+  for (var i = 0; i < WIZARD_QUANTITY; i++) {
+    newWizard = {
+      name: generateRandomParameter(WIZARD_NAMES) + ' ' + generateRandomParameter(WIZARD_SECOND_NAMES),
+      coatColor: generateRandomParameter(WIZARD_COAT_COLOR),
+      eyesColor: generateRandomParameter(WIZARD_EYES_COLOR)
+    };
+    arr[i] = newWizard;
+  }
+  return arr;
+};
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var addWizardsInDOM = function () {
+  var fragment = document.createDocumentFragment();
+  generateRandomAppearance(wizards);
+  for (var j = 0; j < wizards.length; j++) {
+    fragment.appendChild(renderWizard(wizards[j]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+addWizardsInDOM();
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
