eef125d6b92143eeba09e124ce7926a1fb8a95f5
Julia <julia.volkova090@gmail.com>
2018-02-12 20:03:52 +0300

    Finish module4-task1 4

--- js/setup.js
+++ js/setup.js
@@ -63,8 +63,11 @@ var setupClose = setup.querySelector('.setup-close');
 var setupWizard = setup.querySelector('.setup-wizard');
 var setupUserName = setup.querySelector('.setup-user-name');
 var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardCoatInput = document.querySelector('input[name="coat-color"]');
 var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var wizardEyesInput = document.querySelector('input[name="eyes-color"]');
 var setupFireball = setup.querySelector('.setup-fireball-wrap');
+var setupFireballInput = document.querySelector('input[name="fireball-color"]');
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -102,15 +105,19 @@ setupClose.addEventListener('keydown', function (evt) {
   }
 });
 
-var changeColorLook = function (element, array) {
+var changeColorLook = function (element, array, input) {
   element.addEventListener('click', function () {
-    element.style.fill = generateRandomParameter(array);
+    var color = generateRandomParameter(array);
+    element.style.fill = color;
+    input.value = color;
   });
 };
 
-changeColorLook(wizardCoat, coatColors);
-changeColorLook(wizardEyes, eyesColors);
+changeColorLook(wizardCoat, coatColors, wizardCoatInput);
+changeColorLook(wizardEyes, eyesColors, wizardEyesInput);
 
 setupFireball.addEventListener('click', function () {
-  setupFireball.style.backgroundColor = generateRandomParameter(fireballColors);
+  var color = generateRandomParameter(fireballColors);
+  setupFireball.style.backgroundColor = color;
+  setupFireballInput.value = color;
 });
