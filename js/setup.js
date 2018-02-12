22404d2e254a252e3fb2bf95803117174df4e8eb
Julia <julia.volkova090@gmail.com>
2018-02-12 15:44:47 +0300

    Finish module4-task1 3

--- js/setup.js
+++ js/setup.js
@@ -61,6 +61,7 @@ var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
 var setupWizard = setup.querySelector('.setup-wizard');
+var setupUserName = setup.querySelector('.setup-user-name');
 var wizardCoat = setupWizard.querySelector('.wizard-coat');
 var wizardEyes = setupWizard.querySelector('.wizard-eyes');
 var setupFireball = setup.querySelector('.setup-fireball-wrap');
@@ -68,7 +69,7 @@ var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var onPopupEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ESC_KEYCODE && setupUserName !== document.activeElement) {
     closePopup();
   }
 };
